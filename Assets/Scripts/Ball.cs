7700ff3bf8cab43e466d27b1487ea6f1758dbc32
Eric Mesa <ericsbinaryworld@gmail.com>
2019-10-14 20:25:14 -0400

    ball now launches on click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,12 @@ public class Ball : MonoBehaviour
 {
     //Configuration parameters
     [SerializeField] Paddle paddle1; //allows you to attach the paddle to the ball in Unity via drag & drop
-
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    
     // State
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,24 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
