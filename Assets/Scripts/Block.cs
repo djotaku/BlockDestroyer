d0ef72ad6aa3d5785279a9852b7dd79c3f6caabf
Eric Mesa <ericsbinaryworld@gmail.com>
2019-10-15 22:36:38 -0400

    added sound to the blocks being destroyed. Added the ability to progress to the next level.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+
+    [SerializeField] AudioClip breakSound;
+
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>(); // a way to link to the level script without using serialize field
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestoryBlock();
+    }
+
+    private void DestoryBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position); // because the listener is at the camera
         Destroy(gameObject);
+        level.BLockDestroyed();
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // parameters
+    [SerializeField] int breakableBlocks; // serialized for debugging purposes - not to change it in Unity
+
+    // cached reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>(); // grabs the Scenloader from this level
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BLockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+}
