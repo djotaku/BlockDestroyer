ae2d4e16bb60236c35cc72d5f67423d96e761bf0
Eric Mesa <ericsbinaryworld@gmail.com>
2019-10-17 17:03:29 -0400

    Better array handling with blocks
    In order to not have a mismatch between broken sprites and max hits, we've linked it in code instead of using a serialized field now. Also, made a fix that will let us know if we forgot to add in sprites for damage.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,7 +8,6 @@ public class Block : MonoBehaviour
     // config params 
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
-    [SerializeField] int maxHits;
     [SerializeField] Sprite[] hitSprites;
 
     // cached reference
@@ -42,6 +41,7 @@ public class Block : MonoBehaviour
     private void HandleHit()
     {
         timesHit++;
+        int maxHits = hitSprites.Length + 1;
         if (timesHit >= maxHits) // >= in case somehow before it does this, it's gone past the maxHits value
         {
             DestroyBlock();
@@ -55,7 +55,14 @@ public class Block : MonoBehaviour
     private void ShowNextHitSprite() // this is used for affordance - or making the block change appearance as it gets hit
     {
         int spriteIndex = timesHit - 1;
-        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block Sprit is missing from array" + gameObject.name); // helps in case we mess up with filling the array with sprites - will tell us which block is wrong
+        }
     }
 
     private void DestroyBlock()
