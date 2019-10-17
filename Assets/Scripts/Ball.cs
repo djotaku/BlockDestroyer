60f8cc97f3fab6f19115a2ca5475e70286013100
Eric Mesa <ericsbinaryworld@gmail.com>
2019-10-17 17:59:18 -0400

    ending boring ball loops
    It sucks when it's going left and right, slowly until it finally get to the blocks! So some tuning to fix that.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
@@ -10,6 +7,7 @@ public class Ball : MonoBehaviour
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
 
     // State
     Vector2 paddleToBallVector;
@@ -17,12 +15,14 @@ public class Ball : MonoBehaviour
 
     // Cached Componenet references
     AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D; // physics on the ball happens with the RigidBody2D so we are caching this here so we can mess with the physics; want to cache if you're doing a get component more than once
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -39,7 +39,7 @@ public class Ball : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            myRigidBody2D.velocity = new Vector2(xPush, yPush);
             hasStarted = true;
         }
     }
@@ -52,10 +52,15 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2
+            (Random.Range(0, randomFactor), 
+            Random.Range(0, randomFactor));
+
         if (hasStarted)
         {
             AudioClip clip = ballSounds[UnityEngine.Random.Range(0,ballSounds.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
         }
     }
 }
